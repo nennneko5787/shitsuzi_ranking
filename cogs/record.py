316e115ce1cc5e9963bf319376bdbbbbe4834959
ねんねこ <nennneko5787@gmail.com>
2025-03-19 22:52:34 +0900

    record.py を更新

--- cogs/record.py
+++ cogs/record.py
@@ -4,6 +4,23 @@ from discord.ext import commands
 class RecordCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
+
+    @commands.Cog.listener()
+    async def on_guild_channel_create(channel: discord.TextChannel):
+        if channel.name != "1day-chat":
+            return
+
+        def check(m):
+            return m.author.id == 1178247997465837588 and m.channel == channel
+
+        await self.bot.wait_for("message", check=check)
+
+        def check(m):
+            return m.channel == channel
+
+        message = await self.bot.wait_for("message", check=check)
+
+        await channel.send(f"{message.author.mention} さんがコインロールを手に入れました！")
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(RecordCog(bot))
