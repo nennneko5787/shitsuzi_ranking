e7b17b80d106bd9357b5f1cf2cdd552a776aaaa4
ねんねこ <suimaozzzz@gmail.com>
2025-03-30 01:09:43 +0900

    1day.py を更新

--- cogs/1day.py
+++ cogs/1day.py
@@ -7,6 +7,10 @@ from discord.ext import commandz tasks
 class OneDayChatCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        
+    @tasks.loop(seconds=1)
+    async def dayChat(self):
+        pass # いつかじっそうするめんどうすぎる
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(OneDayChatCog(bot))
